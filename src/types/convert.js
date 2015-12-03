865a7f6ec91197d4772cd4bf92c14d54913b2888
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-12-03 13:36:25 +0100

    Add converters for both little- and big-endian types.

--- src/types/convert.js
+++ src/types/convert.js
@@ -1,48 +1,40 @@
+function nativeConverter (method, length) {
+	return function (name) {
+		this.data.setValue(name, this.buffer[method](this.pointer))
+		this.pointer += length
+	}
+}
+
+function native64BitConverter (littleEndian, unsigned) {
+	return function (name) {
+		var sign  = (unsigned || this.buffer[this.pointer] & 0x80 === 0x80 ? 1 : -1)
+		var value = (Math.pow(2, 32) * this.buffer.readInt32BE (this.pointer + (littleEndian ? 4 : 0))) +
+					(sign            * this.buffer.readUInt32BE(this.pointer + (littleEndian ? 0 : 4)))
+		this.data.setValue(name, value)
+		this.pointer += 8
+	}
+}
+
 export default {
-	int8 (name) {
-		this.data.setValue(name, this.buffer.readInt8(this.pointer))
-		this.pointer += 1
-	},
-	uint8 (name) {
-		this.data.setValue(name, this.buffer.readUInt8(this.pointer))
-		this.pointer += 1
-	},
-	int16 (name) {
-		this.data.setValue(name, this.buffer.readInt16BE(this.pointer))
-		this.pointer += 2
-	},
-	uint16 (name) {
-		this.data.setValue(name, this.buffer.readUInt16BE(this.pointer))
-		this.pointer += 2
-	},
-	int32 (name) {
-		this.data.setValue(name, this.buffer.readInt32BE(this.pointer))
-		this.pointer += 4
-	},
-	uint32 (name) {
-		this.data.setValue(name, this.buffer.readUInt32BE(this.pointer))
-		this.pointer += 4
-	},
-	int64 (name) {
-		var value = (Math.pow(2, 32) * this.buffer.readInt32BE(this.pointer)) +
-			((this.buffer[this.pointer] & 0x80 === 0x80 ? 1 : -1) * this.buffer.readUInt32BE(this.pointer + 4))
-		this.data.setValue(name, value)
-		this.pointer += 8
-	},
-	uint64 (name) {
-		var value = (Math.pow(2, 32) * this.buffer.readInt32BE(this.pointer)) +
-			(this.buffer.readUInt32BE(this.pointer + 4))
-		this.data.setValue(name, value)
-		this.pointer += 8
-	},
-	'float' (name) {
-		this.data.setValue(name, this.buffer.readFloatBE(this.pointer))
-		this.pointer += 8
-	},
-	'double' (name) {
-		this.data.setValue(name, this.buffer.readDoubleBE(this.pointer))
-		this.pointer += 8
-	},
+	int8: nativeConverter('readInt8', 1),
+	uint8: nativeConverter('readUInt8', 1),
+	int16le: nativeConverter('readInt16LE', 2),
+	int16be: nativeConverter('readInt16BE', 2),
+	uint16le: nativeConverter('readUInt16LE', 2),
+	uint16be: nativeConverter('readUInt16BE', 2),
+	int32le: nativeConverter('readInt32LE', 4),
+	int32be: nativeConverter('readInt32BE', 4),
+	uint32le: nativeConverter('readUInt32LE', 4),
+	uint32be: nativeConverter('readUInt32BE', 4),
+	int64le: native64BitConverter(true, false),
+	int64be: native64BitConverter(false, false),
+	uint64le: native64BitConverter(true, true),
+	uint64be: native64BitConverter(false, true),
+	floatle: nativeConverter('readFloatLE', 8),
+	floatbe: nativeConverter('readFloatBE', 8),
+	doublele: nativeConverter('readDoubleLE', 8),
+	doublebe: nativeConverter('readDoubleBE', 8),
+
 	integer (name, length) {
 		length = parseInt(length)
 		var value = parseInt(this.buffer.toString('hex', this.pointer, this.pointer + length), 16)
