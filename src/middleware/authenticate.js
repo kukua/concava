c3609cbe9d33222f2bd5c466b874f4943094143f
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-12-22 14:34:27 +0100

    Make request header optional in authentication middleware.

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,31 +1,36 @@
 export default (config) => {
 	if ( ! config.enabled) return (req, res, next) => { next() }
 
-	var header = (config.header || 'Authorization').toLowerCase()
+	var verifyHeader = ( !! config.header)
+	var header = (config.header || '').toLowerCase()
 	var tokenLength = 32
 	var tokenRegExp = new RegExp(`^Token ([a-zA-Z0-9]{${tokenLength}})$`)
 
 	return (req, res, next) => {
-		var value = req.headers[header]
 		var err
-		req.auth = { header: value }
 
-		if ( ! req.auth.header) {
-			err = new Error(`Missing ${config.header} header.`)
-			err.statusCode = 401
-			return next(err)
-		}
+		req.auth = {}
 
-		if (config.byToken) {
-			var token = value.replace(tokenRegExp, '$1')
+		if (verifyHeader) {
+			var value = req.auth.header = req.headers[header]
 
-			if (token.length !== tokenLength) {
-				err = new Error('Invalid token.')
+			if ( ! value) {
+				err = new Error(`Missing ${config.header} header.`)
 				err.statusCode = 401
 				return next(err)
 			}
 
-			req.auth.token = token
+			if (config.byToken) {
+				var token = value.replace(tokenRegExp, '$1')
+
+				if (token.length !== tokenLength) {
+					err = new Error('Invalid token.')
+					err.statusCode = 401
+					return next(err)
+				}
+
+				req.auth.token = token
+			}
 		}
 
 		if ( ! config.method) return next()
