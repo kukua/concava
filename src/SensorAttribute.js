34b97277025fff67cb14315336976935a9997478
Maurits van Mastrigt <maurits@nerdieworks.nl>
2016-05-03 16:32:58 +0200

    Make SensorAttribute methods chainable.

--- src/SensorAttribute.js
+++ src/SensorAttribute.js
@@ -7,35 +7,42 @@ export default class SensorAttribute {
 	}
 	setName (name) {
 		this._name = name
+		return this
 	}
 	getName () {
 		return this._name
 	}
 	setConverters (converters) {
 		this._converters = converters
+		return this
 	}
 	getConverters () {
 		return this._converters
 	}
 	setCalibrators (calibrators) {
 		this._calibrators = calibrators
+		return this
 	}
 	getCalibrators () {
 		return this._calibrators
 	}
 	setValidators (validators) {
 		this._validators = validators
+		return this
 	}
 	getValidators () {
 		return this._validators
 	}
 	addConverter (type, value) {
 		this._converters.push({ type, value })
+		return this
 	}
 	addCalibrator (fn) {
 		this._calibrators.push(fn)
+		return this
 	}
 	addValidator (type, value) {
 		this._validators.push({ type, value })
+		return this
 	}
 }
