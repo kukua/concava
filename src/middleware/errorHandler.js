dd5eba45fd58a30b10c9fc07dc201579d27b8c0e
Maurits van Mastrigt <maurits@nerdieworks.nl>
2016-06-01 11:37:26 +0200

    Add sensor data information to error logging (resolve #27).

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,11 +1,21 @@
 export default (byToken) => {
 	return (err, req, res, next) => {
-		req.log.error({ stack: err.stack }, '' + err)
+		// Log
+		var info = { stack: err.stack }
+		if (req.data) {
+			info.deviceId = req.data.getDeviceId()
+			info.data = req.data.getData()
+		} else if (req.buffer) {
+			info.buffer = req.buffer
+		}
+		req.log.error(info, '' + err)
 
+		// Set response status code
 		var statusCode = (err.statusCode || 500)
 		if (statusCode === 401 && byToken) res.setHeader('WWW-Authenticate', 'Token')
 		res.writeHead(statusCode)
 
+		// Output error message
 		if (err instanceof Error) err = err.toString().replace(/^Error: /, '')
 
 		if (statusCode === 500) {
@@ -13,6 +23,7 @@ export default (byToken) => {
 		} else {
 			res.end(err || '')
 		}
+
 		next()
 	}
 }
